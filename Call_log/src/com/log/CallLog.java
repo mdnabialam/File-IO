28dfc731f94660b6e78cafd6e4efea43fb510ba5
nabispring <nabijavamd@gmail.com>
2023-08-26 14:42:24 +0530

    Call Log Program

---
+++ Call_log/src/com/log/CallLog.java
@@ -0,0 +1,11 @@
+package com.log;
+
+public class CallLog {
+		int start;
+		int end;
+		
+		public CallLog (int start, int end) {
+			this.start=start;
+			this.end=end;
+		}
+}
---
+++ Call_log/src/com/log/CallProgram.java
@@ -0,0 +1,12 @@
+package com.log;
+
+public class CallProgram {
+	int time;
+	boolean start;
+	
+	public CallProgram(int time, boolean start) {
+		this.time=time;
+		this.start=start;
+	}
+	
+}
